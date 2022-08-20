66239cd4bdbc067924839f1d8aac32e6446c8416
Keith Holdaway <keithholdaway1@hotmail.com>
2022-08-20 13:46:42 -0700

    Console working, almsot compleate

--- calc.js
+++ calc.js
@@ -15,6 +15,32 @@ reader.question("What would you like to calculate?", function(input){
 	const num2 = Number(tokens[2]);
 
 
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
+	// Add
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+	}
+	// Sub
+	if (mathSymbol === "-"){
+		console.log(num1 - num2);
+	}
+	//Mult
+	if (mathSymbol === "*"){
+		console.log(num1 * num2);
+	}
+	//Divide
+	if (mathSymbol === "/"){
+		console.log(num1 / num2);
+	}
+	//Sqrt
+	if (mathSymbol === "^"){
+		console.log(Math.sqrt(num1));
+	}
+	
+
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
